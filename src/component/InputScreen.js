eea40a99b1cc29038fb4e30e2bccd0398150ac3c
Vijay Chendrayan Chandrasekar <vijaychendrayan@gmail.com>
2023-04-13 11:40:30 +0530

    Working PFX file version

--- src/component/InputScreen.js
+++ src/component/InputScreen.js
@@ -1,21 +1,121 @@
+// import * as asn1js from 'https://unpkg.com/asn1js@latest?module';
+// import * as pvtsutils from 'https://unpkg.com/pvtsutils@latest?module';
+// import * as pkijs from '../pkijs.es.js';
+// import * as pvutils from 'https://unpkg.com/pvutils?module';
+
+import * as asn1js from 'asn1js';
+import * as pvtsutils from 'pvtsutils';
+import * as pkijs from 'pkijs';
+import * as pvutils from 'pvutils';
+
+
 import { useState } from "react";
 
-
-function createPFXFile({key,cert}){
-    console.log('Button Clicked')
-    console.log('Key - ',{key})
-    console.log('Certificate - ', {cert})
+function destroyClickedElement(event) {
+    document.body.removeChild(event.target);
 }
+
+function saveFile(result) {
+    const pkcs12AsBlob = new Blob([result], { type: "application/x-pkcs12" });
+    const downloadLink = document.createElement("a");
+    downloadLink.download = "pkijs_pkcs12.p12";
+    downloadLink.innerHTML = "Download File";
+    downloadLink.href = window.URL.createObjectURL(pkcs12AsBlob);
+    downloadLink.onclick = destroyClickedElement;
+    downloadLink.style.display = "none";
+    document.body.appendChild(downloadLink);
+    downloadLink.click();
+}
+
+async function generateCertificatePFX1(key,cert){
+    console.log('-----inside generateCertificate------')
+    console.log('Key : ',key);
+    console.log('Cert : ',cert);
+    const certificateBASE64 = "MIICajCCAcwCFDynpN5Ssw9J1++fMnasb87rfJrgMAoGCCqGSM49BAMCMHQxCzAJBgNVBAYTAklOMRAwDgYDVQQIDAdHVUpBUkFUMQ8wDQYDVQQHDAZWQUxTQUQxCzAJBgNVBAoMAlBQMQswCQYDVQQLDAJQUDELMAkGA1UEAwwCUFAxGzAZBgkqhkiG9w0BCQEWDHBwQGdtYWlsLmNvbTAeFw0yMzA0MDQwOTU4MzZaFw0yMzA1MDQwOTU4MzZaMHQxCzAJBgNVBAYTAklOMRAwDgYDVQQIDAdHVUpBUkFUMQ8wDQYDVQQHDAZWQUxTQUQxCzAJBgNVBAoMAkFQMQswCQYDVQQLDAJBUDELMAkGA1UEAwwCQVAxGzAZBgkqhkiG9w0BCQEWDGFwQGdtYWlsLmNvbTCBmzAQBgcqhkjOPQIBBgUrgQQAIwOBhgAEAAuJXGrhINQZRFVKeCTw1Wu2ote3HYXfTpuscRSicZjFRJchjTVDecQEAdiURmwKO4oBtQTfnjLWn1iw8AjNu58/AePKbpoHPO/0BH/5IeZ+XhdQELS2QbyQb8C8ZIzOayAHZkyu3Osuyo63j2/JFHI7k22CKjUeTuo+lWdvag9cmX4PMAoGCCqGSM49BAMCA4GLADCBhwJBVCgEy9cJdHpmbgQSUdutgoARyWxDgOR9RXxeYcvoQbq8epUpwc6nnJfQpgGQuhhUNizEaTedMA1vGNs5QRu3fBwCQgGtXm7H/YEbv+SbmRZLS/EsNqKCJKqqmNIjKItrvsmoktHELDg+ciS67iGEdqKH1RW5a/m0Vr9L5u8Eq6U4WZEa0A=="
+    const privateKeyBASE64 = "MIHcAgEBBEIApLmnlJFKswUbZzxrVfT5YcUHt+uISytpBXn+L9KJCSQjv8AP/heJxoJoX6ghqpXmBcaj6MEU1yPBH7vZjAcPr+igBwYFK4EEACOhgYkDgYYABAALiVxq4SDUGURVSngk8NVrtqLXtx2F306brHEUonGYxUSXIY01Q3nEBAHYlEZsCjuKAbUE354y1p9YsPAIzbufPwHjym6aBzzv9AR/+SHmfl4XUBC0tkG8kG/AvGSMzmsgB2ZMrtzrLsqOt49vyRRyO5Ntgio1Hk7qPpVnb2oPXJl+Dw=="
+    const password = 'welogical'
+     //#region Create simplified structires for certificate and private key
+     const certRaw = pvutils.stringToArrayBuffer(pvutils.fromBase64(certificateBASE64));
+     const certSimpl = pkijs.Certificate.fromBER(certRaw);
+     const pkcs8Raw = pvutils.stringToArrayBuffer(pvutils.fromBase64(privateKeyBASE64));
+    //  const pkcs8Simpl = pkijs.PrivateKeyInfo.fromBER(pkcs8Raw);
+     const pkcs8Simpl = pkijs.ECPrivateKey.fromBER(pkcs8Raw);
+     //#endregion
+     //#region Put initial values for PKCS#12 structures
+     const pkcs12 = new pkijs.PFX({
+         parsedValue: {
+             integrityMode: 0,
+             authenticatedSafe: new pkijs.AuthenticatedSafe({
+                 parsedValue: {
+                     safeContents: [
+                         {
+                             privacyMode: 2,
+                             value: new pkijs.SafeContents({
+                                 safeBags: [
+                                     new pkijs.SafeBag({
+                                         bagId: "1.2.840.113549.1.12.10.1.1",
+                                         bagValue: pkcs8Simpl
+                                     }),
+                                     new pkijs.SafeBag({
+                                         bagId: "1.2.840.113549.1.12.10.1.3",
+                                         bagValue: new pkijs.CertBag({
+                                             parsedValue: certSimpl
+                                         })
+                                     })
+                                 ]
+                             })
+                         }
+                     ]
+                 }
+             })
+         }
+     });
+     //#endregion
+     //#region Encode internal values for all "SafeContents" firts (create all "Privacy Protection" envelopes)
+     if (!(pkcs12.parsedValue && pkcs12.parsedValue.authenticatedSafe)) {
+         throw new Error("pkcs12.parsedValue.authenticatedSafe is empty");
+     }
+     await pkcs12.parsedValue.authenticatedSafe.makeInternalValues({
+         safeContents: [
+             {
+                 encryptingCertificate: certSimpl,
+                 encryptionAlgorithm: {
+                     name: "AES-CBC",
+                     length: 128
+                 }
+             }
+         ]
+     });
+     //#endregion
+     //#region Encode internal values for "Integrity Protection" envelope
+     await pkcs12.makeInternalValues({
+         password: pvutils.stringToArrayBuffer(password),
+         iterations: 100000,
+         pbkdf2HashAlgorithm: "SHA-256",
+         hmacHashAlgorithm: "SHA-256"
+     });
+     //#endregion
+     //#region Save encoded data
+     console.log(pkcs12.toString())
+     return pkcs12.toSchema().toBER(false);
+     //#endregion
+}
+
+
+
 
 
 function InputScreen(){
     const [key, setKey] = useState('');
     const [cert, setCert] = useState('');
 
-    function createPFXFile(){
+    async function createPFXFile(){
         console.log('Button Clicked')
         console.log('Key - ',key)
         console.log('Certificate - ', cert)
+        // generateCertificatePFX(key,cert);
+        const pfx = await generateCertificatePFX1(key,cert);
+        saveFile(pfx)
     }
 
     
